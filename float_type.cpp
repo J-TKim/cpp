db8c6bf4ccdd089bb65865d0bf6c3ccb735aa763
Kim, Jeong Tae <68172322+J-TKim@users.noreply.github.com>
2021-04-28 09:55:43 +0900

    Create float_type.cpp

---
+++ float_type.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    // 실수형 : 소수부가 있는 수
+
+    float a = 3.14;
+    int b = 3.14;
+
+    cout << a << " / " << b << endl;
+
+    return 0;
+}
