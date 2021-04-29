6ad678ea3b6ecd74f5d15c5e214e69cf43b07c5b
Kim, Jeong Tae <68172322+J-TKim@users.noreply.github.com>
2021-04-29 09:29:51 +0900

    Create bool.cpp

---
+++ bool.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // bool : 0 혹은 1
+    bool a = 0;
+    bool b = 1;
+    bool c = 10;
+    bool d = -1;
+
+    cout << "a : " << a << endl;
+    cout << "b : " << b << endl;
+    cout << "c : " << c << endl;
+    cout << "d : " << d << endl;
+
+    return 0;
+}
