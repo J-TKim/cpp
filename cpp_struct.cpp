e8c2dba21ea4ef5b77b75a45a2fd07c19bee9a9e
Kim, Jeong Tae <68172322+J-TKim@users.noreply.github.com>
2021-04-30 15:34:58 +0900

    Update cpp_struct.cpp

--- cpp_struct.cpp
+++ cpp_struct.cpp
@@ -13,9 +13,38 @@ int main() {
         string position;
         float height;
         float weight;
-    };
+    } B;
 
     MyStruct A;
+
+    A.name = "Son";
+    A.position = "Forward";
+    A.height = 183;
+    A.weight = 77;
+
+    /*
+    MyStruct A = {
+        "Son",
+        "Striker",
+        183,
+        77
+    };
+    */
+
+    cout << A.name << endl;
+    cout << A.position << endl;
+    cout << A.height << endl;
+    cout << A.weight << endl;
+
+    B = {};
+    cout << B.height << endl;
+
+    MyStruct C[2] = {
+        {"A", "A", 1, 1},
+        {"B", "B", 2, 2}
+    };
+
+    cout << C[0].height << endl;
 
     return 0;
 }
